36a9b4357cb16a24604a061cbb772695671ecc2b
Anna Maria <anna.maria.dev.br@gmail.com>
2026-04-02 12:38:29 -0300

    feat: add new logo image to public directory

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -4,6 +4,7 @@ import { Theme } from "@radix-ui/themes"
 import { useState } from "react"
 import { Moon, Sun } from "lucide-react"
 import { IconButton } from "@radix-ui/themes"
+import Image from "next/image"
 import Link from "next/link"
 import { Toaster } from "sonner"
 
@@ -12,19 +13,17 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
 
   return (
     <Theme appearance={appearance}>
-      <header className="w-full p-4 flex items-center justify-between">
-        <div className="flex-1" />
+      <header className="px-5 py-3 border-b border-zinc-100 dark:border-zinc-800 flex items-center justify-between">
+        <Image src="/logo.png" alt="Logo" width={50} height={50} className="object-contain" />
         <Link href="/" className="font-bold text-xl">resend-ecommerce</Link>
-        <div className="flex-1 flex justify-end">
-          <IconButton
-            variant="ghost"
-            color="gray"
-            highContrast
-            onClick={() => setAppearance(appearance === "dark" ? "light" : "dark")}
-          >
-            {appearance === "dark" ? <Sun size={16} /> : <Moon size={16} />}
-          </IconButton>
-        </div>
+        <IconButton
+          variant="ghost"
+          color="gray"
+          highContrast
+          onClick={() => setAppearance(appearance === "dark" ? "light" : "dark")}
+        >
+          {appearance === "dark" ? <Sun size={16} /> : <Moon size={16} />}
+        </IconButton>
       </header>
       {children}
       <Toaster
